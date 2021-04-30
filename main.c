584ae913a3a7dac7d386b21ca64655d600806c60
beetrootsys <80077386+beetrootsys@users.noreply.github.com>
2021-04-30 09:42:35 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+//#include <stdio.h>
+
 
 #include "geometry.h"
 
 int main(void) {
-	printf("Hello World\n");
+	/*printf("Hello World\n"); //Debugging/testing stuff
 	printf("pi is equal to %lf\n", pi);
 	printf("8 + 2 is %i\n", 8+2);
 	printf("8 - 2 is %i\n", 8-2);
 	printf("8 * 2 is %i\n", 8*2);
 	printf("8 / 2 is %i\n", 8/2);
-	printf("2 squared is %lf\n", square(2));
+	printf("2 squared is %lf\n", square(2));*/
 	return 0;
 }
