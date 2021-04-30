98c0f0c3fab1629255d083e2de56c8027ed4aa1a
beetrootsys <80077386+beetrootsys@users.noreply.github.com>
2021-04-30 09:41:43 -0700

    added new functions and fixed old ones

--- geometry.h
+++ geometry.h
@@ -1,11 +1,15 @@
 #include "consts.h"
 
-double square_area(double s) {
+double get_square_area(double s) {
 	return s*s;
 }
 
-double triangle_area(double b, double h/*base and height*/) {
+double get_triangle_area(double b, double h/*base and height*/) {
 	return b*h / 2;
+}
+
+double get_circle_area(double r/*radius*/) {
+	return pi * square(r); //A = π(2r)
 }
 
 double get_radius(double d/*diameter*/) {
@@ -16,6 +20,10 @@ double get_diameter(double r/*radius*/)  {
 	return r*2;
 }
 
-double get_circumfrence(double r/*radius*/) {
-	return pi * square(r); //C = π(2r)
+double get_circumfrence_fromr(double r/*radius*/) {
+	return (2*pi) * r; //C = π(2r)
+}
+
+double get_circumfrence_fromd(double d/*diameter*/) {
+	return pi * d; //C = 2πr
 }
